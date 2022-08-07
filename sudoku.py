396b9c62b63cb735c46b2af8e6944000d692f293
pop-pontus <pontus.hovberger@gmail.com>
2022-08-07 11:52:57 +0200

    Guess & check if valid

--- sudoku.py
+++ sudoku.py
@@ -19,4 +19,24 @@ while sudoku[r][c] != 0:            # Loop through to sudoku until unsolved cell
     else:
         c += 1
 
+def guess(r, c):
+    for guess in range(1,9):
+        if valid_guess(r, c, guess):
+            print(guess)
+
+def valid_guess(r, c, guess):
+    for check_row in range(0,8):                                        # Check if guess is in colummn
+        if sudoku[check_row][c] == guess and check_row != r:
+            return False
+    
+    for check_column in range(0,8):                                     # Check if guess is in row
+        if sudoku[r][check_column] == guess and check_column != c:
+            return False
+
+    square_row = r // 3
+    square_column = c // 3
+    print(square_row, square_column)
+    
+    return True
+
 print(sudoku)
