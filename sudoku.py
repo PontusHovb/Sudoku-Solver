e248e1af04a24317c21a56f5778cbe7c26774cb7
pop-pontus <pontus.hovberger@gmail.com>
2022-08-07 12:06:44 +0200

    Structure code

--- sudoku.py
+++ sudoku.py
@@ -11,32 +11,47 @@ sudoku = np.array(
     [0, 0, 0, 4, 1, 9, 0, 0, 5],
     [0, 0, 0, 0, 8, 0, 0, 7, 9]]    )       # Start array
 
-r = c = 0
-while sudoku[r][c] != 0:            # Loop through to sudoku until unsolved cell is found
-    if c == 8:                      # Skip to next row if end of row
-        c = 0
-        r += 1
-    else:
-        c += 1
 
-def guess(r, c):
-    for guess in range(1,9):
-        if valid_guess(r, c, guess):
+def find_unsolved_cell(sudoku):
+    r = c = 0
+    while sudoku[r][c] != 0:            # Loop through to sudoku until unsolved cell is found
+        if c == 8:                      # Skip to next row if end of row
+            c = 0
+            r += 1
+        else:
+            c += 1
+    
+    return r, c
+
+def guess(r, c, sudoku):
+    for guess in range(1,10):
+        if valid_guess(r, c, guess, sudoku):
             print(guess)
 
-def valid_guess(r, c, guess):
-    for check_row in range(0,8):                                        # Check if guess is in colummn
+def valid_guess(r, c, guess, sudoku):
+    for check_row in range(0,9):                                        # Check if guess is in colummn
         if sudoku[check_row][c] == guess and check_row != r:
             return False
     
-    for check_column in range(0,8):                                     # Check if guess is in row
+    for check_column in range(0,9):                                     # Check if guess is in row
         if sudoku[r][check_column] == guess and check_column != c:
             return False
 
-    square_row = r // 3
-    square_column = c // 3
-    print(square_row, square_column)
-    
-    return True
+    for check_row in range(1, 4):                                       # Check if guess is in square
+        for check_column in range(1, 4):
+            if r // 3 + check_row != r and c // 3 + check_column != c:
+                if sudoku[r // 3 + check_row][c // 3 + check_column] == guess:
+                    return False
 
-print(sudoku)
+    return True
+    
+
+
+def main():
+    r, c = find_unsolved_cell(sudoku)
+    print("cell", r, c)
+    guess(r, c, sudoku)
+    print(sudoku)
+
+if __name__ == '__main__':
+    main()
