672a9f887c776cced118a144ad2c1a47283eb04c
PontusHovb <pontus.hovberger@gmail.com>
2024-06-02 15:48:22 +0200

    Created class for sudoku in python

---
+++ solver.py
@@ -0,0 +1,130 @@
+import numpy as np
+
+class Sudoku:
+    def __init__(self, puzzle, solution):
+        self.puzzle = puzzle
+        self.solution = solution
+        self.unsolved_cells = self.get_all_unsolved(self.puzzle)
+
+    def __str__(self):
+        for r, row in enumerate(self.puzzle):
+            if r in [3, 6]:
+                print('------+-------+------')
+            for c, value in enumerate(row):
+                if c in [3, 6]:
+                    print('|', end=' ')
+                print(value, end=' ')
+
+            print('\n', end='')
+            
+    def solve(self, method):
+        if method == "bruteforce":                                       
+            return self.bruteforce(self.puzzle, self.unsolved_cells)
+        elif method == "bruteforce_lookahead":
+            return self.bruteforce_lookahead(self.puzzle, self.unsolved_cells)
+        elif method == "candidate_checking":
+            return self.candidate_checking(self.puzzle, self.unsolved_cells)
+        elif method == "place_finding":
+            return self.place_finding(self.puzzle, self.unsolved_cells)
+        else:
+            print("Enter valid algorithm")
+    
+    def correct_solution(self):
+        return np.array_equal(self.puzzle, self.solution)
+
+    def get_all_unsolved(self, puzzle):
+        unsolved_cells = list()
+        for r, row in enumerate(puzzle):
+            for c, cell in enumerate(row):
+                if cell == 0:
+                    unsolved_cells.append((r, c))
+        
+        return unsolved_cells
+    
+    def valid_guess(self, r, c, guess, puzzle):
+        # Check if guess is in column
+        if guess in puzzle[r] and c != np.where(puzzle[r] == guess):
+            return False
+        
+        # Check if guess is in row
+        if guess in [puzzle[i][c] for i in range(9)] and r != [puzzle[i][c] for i in range(9)].index(guess):
+            return False
+
+        # Check if guess is in square
+        start_row, start_col = 3 * (r // 3), 3 * (c // 3)
+        for check_row in range(start_row, start_row + 3):
+            for check_col in range(start_col, start_col + 3):
+                if puzzle[check_row][check_col] == guess and (check_row, check_col) != (r, c):
+                    return False
+
+        return True
+
+    def get_candidates(self, puzzle, row, column):
+        candidates = set(range(1, 10))
+        candidates -= set(puzzle[row])                                          # Eliminate candidates in same row
+        candidates -= set(puzzle[i][column] for i in range(9))                  # Eliminate candidates in same column
+
+        block_row, block_col = 3 * (row // 3), 3 * (column // 3)                # Eliminate candidates in same box
+        for i in range(3):
+            for j in range(3):
+                candidates.discard(puzzle[block_row + i][block_col + j])
+
+        return candidates
+
+    def bruteforce(self, puzzle, empty_cells): 
+        if len(empty_cells) == 0:
+            return True if self.correct_solution() else False
+               
+        for guess in range(1,10):                                               # If empty cells left, make a guess for first empty cell
+            puzzle[empty_cells[0][0]][empty_cells[0][1]] = guess
+
+            if self.bruteforce(puzzle, empty_cells[1:]):                        # Recursive solving
+                return True
+            
+            puzzle[empty_cells[0][0]][empty_cells[0][1]] = 0                    # Backtrack if guess don't yield solution
+
+    def bruteforce_lookahead(self, puzzle, empty_cells):
+        if len(empty_cells) == 0:                                     
+            return True if self.correct_solution() else False
+
+        for guess in range(1, 10):                                              # If empty cells left, make a guess for first empty cell
+            if self.valid_guess(empty_cells[0][0], empty_cells[0][1], guess, puzzle):
+                puzzle[empty_cells[0][0]][empty_cells[0][1]] = guess
+
+                if self.bruteforce_lookahead(puzzle, empty_cells[1:]):          # Recursive solving
+                    return True
+                    
+                puzzle[empty_cells[0][0]][empty_cells[0][1]] = 0                # Backtrack if guess don't yield solution
+
+    def candidate_checking(self, puzzle, empty_cells):
+        if len(empty_cells) == 0:                                               # puzzle solved if there are no more empty cells left
+            return True
+        
+        for empty_cell in empty_cells:                                          # Loop through all empty cells
+            possible_values = 0
+            for guess in range(1, 10):
+                if self.valid_guess(empty_cell[0], empty_cell[1], guess, puzzle):
+                    possible_guess = guess
+                    possible_values += 1
+
+            if possible_values == 1:                                            # If there is only one candidate for empty cell
+                puzzle[empty_cell[0]][empty_cell[1]] = possible_guess
+                empty_cells.remove(empty_cell)
+                return self.candidate_checking(puzzle, empty_cells)
+
+        return False                                                            # Correct value can't be determined for any empty cell
+
+    def place_finding(self, sudoku, empty_cells):
+        if len(empty_cells) == 0:                                               # Sudoku solved if there are no more empty cells left
+            return True
+        
+        for empty_cell in empty_cells:                                          # Loop through all empty cells
+            candidates = self.get_candidates(sudoku, empty_cell[0], empty_cell[1])
+
+            if len(candidates) == 1:                                            # Hidden single if there is only one candidate left
+                sudoku[empty_cell[0]][empty_cell[1]] = candidates.pop()
+                empty_cells.remove(empty_cell)   
+                return self.candidate_checking(sudoku, empty_cells)
+        
+        return False          
+
--- sudoku.py
+++ sudoku.py
@@ -1,159 +1,46 @@
 import numpy as np
 import time
-from itertools import combinations
+from solver import Sudoku
 
-FILENAME = "hard_sudoku.csv"
+FILENAME = "easy_sudoku.csv"
+ALGORITHM = "candidate_checking"
 NO_PUZZLES = 1
+SIZE = 9
+ 
+# Read puzzles from csv-file
+def read_puzzles(filename, no_puzzles):
+    puzzles = np.zeros((no_puzzles, SIZE*SIZE), np.int32)
+    solutions = np.zeros((no_puzzles, SIZE*SIZE), np.int32)
 
-# Read quizzes from csv-file
-def read_quizzes(filename, size):
-    quizzes = np.zeros((size, 81), np.int32)
-    solutions = np.zeros((size, 81), np.int32)
-
-    for i, line in enumerate(open(filename, 'r').read().splitlines()[1:size+1]):
-        quiz, solution = line.split(",")
-        for j, q_s in enumerate(zip(quiz, solution)):
+    for i, line in enumerate(open(filename, 'r').read().splitlines()[1:no_puzzles+1]):
+        puzzle, solution = line.split(",")
+        for j, q_s in enumerate(zip(puzzle, solution)):
             q, s = q_s
-            quizzes[i, j] = q
+            puzzles[i, j] = q
             solutions[i, j] = s
 
-    quizzes = quizzes.reshape((-1, 9, 9))
-    solutions = solutions.reshape((-1, 9, 9))
+    puzzles = puzzles.reshape((-1, SIZE, SIZE))
+    solutions = solutions.reshape((-1, SIZE, SIZE))
 
-    return quizzes, solutions               
-
-
-# Find all unsolved cells in sudoku
-def get_all_unsolved(sudoku):
-    unsolved_cells = list()
-    for r, row in enumerate(sudoku):
-        for c, cell in enumerate(row):
-            if cell == 0:
-                unsolved_cells.append((r, c))
-    
-    return unsolved_cells    
-
-# Check if a guess is valid
-def valid_guess(r, c, guess, sudoku):
-    # Check if guess is in column
-    if guess in sudoku[r] and c != sudoku[r].index(guess):
-        return False
-    
-    # Check if guess is in row
-    if guess in [sudoku[i][c] for i in range(9)] and r != [sudoku[i][c] for i in range(9)].index(guess):
-        return False
-
-    # Check if guess is in square
-    start_row, start_col = 3 * (r // 3), 3 * (c // 3)
-    for check_row in range(start_row, start_row + 3):
-        for check_col in range(start_col, start_col + 3):
-            if sudoku[check_row][check_col] == guess and (check_row, check_col) != (r, c):
-                return False
-
-    return True
-
-def get_candidates(sudoku, row, column):
-    candidates = set(range(1, 10))
-
-    # Eliminate candidates in same row
-    candidates -= set(sudoku[row])
-
-    # Eliminate candidates in same column
-    candidates -= set(sudoku[i][column] for i in range(9))
-
-    # Eliminate candidates in same box
-    block_row, block_col = 3 * (row // 3), 3 * (column // 3)
-    for i in range(3):
-        for j in range(3):
-            candidates.discard(sudoku[block_row + i][block_col + j])
-
-    return candidates
-
-
-###############################################################################################################################
-########################################                  Algorithms                   ########################################
-###############################################################################################################################
-# Brute-force algorithm
-def bruteforce(sudoku, solution, empty_cells):
-    if np.array_equal(sudoku, solution):                                    # Check if sudoku is solved
-        return True
-                                                                  
-    for guess in range(1,10):                                               # If empty cells left, make a guess for first empty cell
-        sudoku[empty_cells[0][0]][empty_cells[0][1]] = guess
-
-        if bruteforce(sudoku, solution, empty_cells[1:]):                   # Recursive solving
-            return True
-            
-        sudoku[empty_cells[0][0]][empty_cells[0][1]] = 0                    # Backtrack if guess don't yield solution
-
-# Brute-force algorithm with look ahead
-def bruteforce_lookahead(sudoku, solution, empty_cells):
-    if len(empty_cells) == 0:                                               # Sudoku solved if there are no more empty cells left
-        return True
-
-    for guess in range(1, 10):
-        if valid_guess(empty_cells[0][0], empty_cells[0][1], guess, sudoku):
-            sudoku[empty_cells[0][0]][empty_cells[0][1]] = guess
-
-            if bruteforce_lookahead(sudoku, solution, empty_cells[1:]):     # Recursive solving
-                return True
-                
-            sudoku[empty_cells[0][0]][empty_cells[0][1]] = 0                # Backtrack if guess don't yield solution
-
-# Candidate-checking method
-def candidate_checking(sudoku, solution, empty_cells):
-    if len(empty_cells) == 0:                                               # Sudoku solved if there are no more empty cells left
-        return True
-    
-    for empty_cell in empty_cells:                                          # Loop through all empty cells
-        possible_values = 0
-        for guess in range(1, 10):
-            if valid_guess(empty_cell[0], empty_cell[1], guess, sudoku):
-                possible_guess = guess
-                possible_values += 1
-
-        if possible_values == 1:                                            # If there is only one candidate for empty cell
-            sudoku[empty_cell[0]][empty_cell[1]] = possible_guess
-            empty_cells.remove(empty_cell)
-            return candidate_checking(sudoku, solution, empty_cells)
-
-    return False                                                            # Correct value can't be determined for any empty cell
-
-# Place-finding method
-def place_finding(sudoku, solution, empty_cells):
-    if len(empty_cells) == 0:                                               # Sudoku solved if there are no more empty cells left
-        return True
-    
-    for empty_cell in empty_cells:                                          # Loop through all empty cells
-        candidates = get_candidates(sudoku, empty_cell[0, empty_cell[1]])
-
-        if len(candidates) == 1:                                            # Hidden single if there is only one candidate left
-            sudoku[empty_cell[0]][empty_cell[1]] = candidates.pop()
-            empty_cells.remove(empty_cell)   
-            return candidate_checking(sudoku, solution, empty_cells)
-    
-    return False
-
-# Choose which algorithm to use
-def solve_sudoku(sudoku, solution):                                         
-    empty_cells = get_all_unsolved(sudoku)
-    return place_finding(sudoku, solution, empty_cells)              
+    return puzzles, solutions    
 
 def main():
     start_time = time.time()
-
-    quizzes, solutions = read_quizzes(FILENAME, NO_PUZZLES)                 # Get sudokus
+    puzzles, solutions = read_puzzles(FILENAME, NO_PUZZLES)                 # Get sudokus
     download_end_time = time.time()
-    print(f"It took {download_end_time - start_time} seconds to download {NO_PUZZLES} sudokus")
+    print(f"It took {round(download_end_time - start_time, 4)} seconds to download {NO_PUZZLES} sudokus")
 
     # Solve each sudoku
     solved_sudokus = 0
-    for sudoku, solution in zip(quizzes, solutions):
-        if solve_sudoku(sudoku, solution):
+    for puzzle, solution in zip(puzzles, solutions):
+        sudoku = Sudoku(puzzle, solution)
+
+        sudoku.solve(ALGORITHM)
+        if sudoku.correct_solution():
             solved_sudokus += 1
 
     solve_end_time = time.time()
-    print(f"Method solved {round(solved_sudokus / NO_PUZZLES, 2)*100}% of {NO_PUZZLES} sudokus in {solve_end_time - download_end_time} seconds")
-    
+    print(f"Method solved {round(solved_sudokus / NO_PUZZLES, 2)*100}% of {NO_PUZZLES} sudokus in {round(solve_end_time - download_end_time, 4)} seconds")
+
 if __name__ == '__main__':
     main()
